280b2e4f49bcb320f328a04cbdbbd34a0a2a9941
Abhishek <graciadeveloper@gmail.com>
2024-09-10 14:48:13 +0530

    ..

--- script.js
+++ script.js
@@ -1,26 +1,4 @@
-// document.addEventListener("DOMContentLoaded", function() {
-//     const videoElement = document.getElementById("heroVideo");
-//     const largeVideoSource = document.getElementById("largeVideoSource");
-//     const mobileVideoSource = document.getElementById("mobileVideoSource");
-  
-//     function setVideoSource() {
-//       if (window.innerWidth <= 768) {
-//         // Mobile video
-//         videoElement.src = mobileVideoSource.src;
-//       } else {
-//         // Large device video
-//         videoElement.src = largeVideoSource.src;
-//       }
-//       videoElement.load(); // Reload the video with the new source
-//     }
-  
-//     // Set video source on page load
-//     setVideoSource();
-  
-//     // Update video source on window resize
-//     window.addEventListener("resize", setVideoSource);
-//   });
-  
+
 
 document.addEventListener("DOMContentLoaded", function() {
   const nav = document.querySelector(".nav");
