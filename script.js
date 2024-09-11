58485019cf20966a3d71adb05be1c805de380864
Abhishek <graciadeveloper@gmail.com>
2024-09-11 17:47:17 +0530

    ..

--- script.js
+++ script.js
@@ -19,22 +19,22 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 
-// window.addEventListener('load', function() {
-//   var videoElement = document.querySelector('.hero-video');
+window.addEventListener('load', function() {
+  var videoElement = document.querySelector('.hero-video');
 
-//   function updatePoster() {
-//     if (window.innerWidth <= 768) {
-//       videoElement.poster = 'https://img.freepik.com/free-photo/mysterious-forest-landscape_181624-35895.jpg?t=st=1726049837~exp=1726053437~hmac=09f0554d9e6051f3171f8dfd543c69fc66dbabec95bf23c657cccdfcdbcae344&w=740';
-//     } else {
-//       videoElement.poster = 'https://img.freepik.com/free-photo/beautiful-japanese-forest-scene_23-2151498135.jpg?t=st=1726054815~exp=1726058415~hmac=01d26b0909c52d64f2cd0667ca97e18d696c6dee84e6c9389edc183a28e36dcf&w=1380';
-//     }
-//   }
-
-
-//   updatePoster();
+  function updatePoster() {
+    if (window.innerWidth <= 768) {
+      videoElement.poster = 'heroMobileImg.jpg';
+    } else {
+      videoElement.poster = 'heroLargeImg.jpg';
+    }
+  }
 
 
-//   window.addEventListener('resize', updatePoster);
-// });
+  updatePoster();
+
+
+  window.addEventListener('resize', updatePoster);
+});
 
 
